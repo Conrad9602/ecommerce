02b1baeb6ee04c6cf6fdc989e95dacb3a10cd948
Conrad9602 <xuanyuzhu1996@gmail.com>
2025-02-28 23:24:12 -0800

    add Product API

--- ecommerce/src/main/java/com/educative/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/educative/ecommerce/model/Product.java
@@ -34,5 +34,66 @@ public class Product {
     public Product() {
 
     }
-// setters and getters
+
+    // setters and getters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", category=" + category +
+                '}';
+    }
 }
